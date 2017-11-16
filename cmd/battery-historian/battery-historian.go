d2e6feff3d84d94a954c958393820193210a4b60
Rafael Francisco <>
2017-11-16 11:34:22 +0000

    Revert "Changed default port to 80"
    This reverts commit d31360e6929ecabe3acb0c8dca39287f3857d4de.

--- cmd/battery-historian/battery-historian.go
+++ cmd/battery-historian/battery-historian.go
@@ -27,7 +27,7 @@ import (
 
 var (
 	optimized = flag.Bool("optimized", true, "Whether to output optimized js files. Disable for local debugging.")
-	port      = flag.Int("port", 80, "service port")
+	port      = flag.Int("port", 9999, "service port")
 
 	compiledDir   = flag.String("compiled_dir", "./compiled", "Directory containing compiled js file for Historian v2.")
 	jsDir         = flag.String("js_dir", "./js", "Directory containing uncompiled js files for Historian v2.")
